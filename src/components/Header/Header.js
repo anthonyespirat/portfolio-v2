03fee9f8223a05e63db1676265db3462bc1c3ea6
anthonyespirat <jayshu666@gmail.com>
2022-01-05 09:36:24 +0100

    add menu mobile

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css'
 import { NavLink } from 'react-router-dom'
+import MobileMenu from './MobileMenu/MobileMenu'
+
+
 
 const Header = () => {
+
+    const [menuIsOpen, setMenuIsOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setMenuIsOpen(menuIsOpen ? false : true)
+        console.log(menuIsOpen);
+    }
+
     return (
+<>
+        <MobileMenu isOpen={menuIsOpen} setIsOpen={setMenuIsOpen}></MobileMenu>
+
         <header className="p-5 flex justify-between">
             {/* LOGO */}
             <div className="logo uppercase text-3xl font-light">Anthony <span className="text-primary">E</span>.</div>
@@ -15,9 +29,10 @@ const Header = () => {
                 
                 <NavLink exact to="/creations" className="px-5 text-gray-400 hover:text-white" activeClassName="current-page"> Mes créations </NavLink>
             </nav>
-            <button id='mobile-menu' className='md:hidden'> <span></span><span></span><span></span> </button>
-            <a href="#contact"><button className="py-2 px-8 border border-gray-400 rounded font-semibold hidden md:block hover:bg-gray-400 hover:bg-opacity-10 transition duration-500">Contact</button></a>
+            <button onClick={() => toggleMenu()} id='mobile-btn' className='md:hidden'> <span></span><span></span><span></span> </button>
+            <a href="#contact" className='hidden md:block'><button className="py-2 px-8 border border-gray-400 rounded font-semibold hidden md:block hover:bg-gray-400 hover:bg-opacity-10 transition duration-500">Contact</button></a>
         </header>
+</>
     );
 };
 
--- src/components/Header/MobileMenu/MobileMenu.js
+++ src/components/Header/MobileMenu/MobileMenu.js
@@ -1,7 +1,23 @@
-const menuMobile = () => {
+import { NavLink } from "react-router-dom"
+
+
+
+const menuMobile = ({isOpen, setIsOpen}) => {
+
+    const removeMenu = () => {
+        setIsOpen(isOpen = false)
+    }
+
     return (
-        <>
-        </>
+        <div className={`mobile-menu overflow-hidden md:hidden block left-0 top-0 ${isOpen ? "active" : ""}`}>
+            <nav className='flex'>
+                <NavLink onClick={() => removeMenu()} exact to="/" className="px-5 text-gray-400 hover:text-white" activeClassName="current-page"> Accueil </NavLink> &bull;
+
+                <a onClick={() => removeMenu()}  href='/#about' className="px-5 text-gray-400 hover:text-white"> À propos </a> &bull;
+                
+                <NavLink onClick={() => removeMenu()} exact to="/creations" className="px-5 text-gray-400 hover:text-white" activeClassName="current-page"> Mes créations </NavLink>
+            </nav>
+        </div> 
     )
 }
 export default menuMobile
